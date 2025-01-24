ac32af50115d2f65454e2516cb72754763e7c2c9
chimfonzee <chimfonzee@chimLAPTOP.>
2025-01-24 23:53:00 +0800

    Added EventListenerExamples

---
+++ EventListenerExamples/src/main/java/com/mycompany/eventlistenerexamples/MyEvent.java
@@ -0,0 +1,9 @@
+package com.mycompany.eventlistenerexamples;
+
+import java.awt.event.*;
+
+public class MyEvent implements ActionListener {
+	public void actionPerformed(ActionEvent e)  {
+		System.out.println("I was clicked!");
+	}
+}
---
+++ EventListenerExamples/src/main/java/com/mycompany/eventlistenerexamples/MyEventToo.java
@@ -0,0 +1,9 @@
+package com.mycompany.eventlistenerexamples;
+
+import java.awt.event.*;
+
+public class MyEventToo implements ActionListener {
+	public void actionPerformed(ActionEvent e)  {
+		System.out.println("I was piggy backing!");
+	}
+}
